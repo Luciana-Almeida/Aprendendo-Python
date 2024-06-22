e8eabc2f7c37adc7952abd0d5d8741fd58c32bfc
LUCIANA EMANUELLE ALMEIDA FERREIRA <159495472+Luciana-Almeida@users.noreply.github.com>
2024-06-22 16:07:31 -0300

    Atualiza os arquivos do repositório

---
+++ Exercícios Python/ex009.py
@@ -0,0 +1,15 @@
+#Faça um programa que leia um número inteiro qualquer e mostre na tela sua tabuada
+nome = input('Olá, qual o seu nome? ')
+v = int(input('De qual número você gostaria de ver a tabuada? '))
+n0 = v * 0
+n1 = v * 1
+n2 = v * 2
+n3 = v * 3
+n4 = v * 4
+n5 = v * 5
+n6 = v * 6
+n7 = v * 7
+n8 = v * 8
+n9 = v * 9
+n10 = v * 10
+print('{} A tabuada de {} é: {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}.' .format(nome, v, n0, n1, n2, n3, n4, n5, n6, n7, n8, n9, n10))
